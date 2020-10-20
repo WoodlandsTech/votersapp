9b0385b8b1f0736b646730a83f16cb67224a8955
Rud Merriam <rud.merriam@gmail.com>
2020-10-20 06:50:35 -0500

    Add Contributors popup source and button on footer

---
+++ src/Contributors.js
@@ -0,0 +1,93 @@
+import React from 'react';
+//import { makeStyles } from '@material-ui/core/styles';
+//import Popover from '@material-ui/core/Popover';
+//import Typography from '@material-ui/core/Typography';
+//import Button from '@material-ui/core/Button';
+
+import { Button, Popover, makeStyles } from '@material-ui/core';
+import { List, ListItemText, ListSubheader } from '@material-ui/core';
+
+
+const useStyles = makeStyles((theme) => ({
+	button: {
+		margin: theme.spacing(5),
+	},
+	typography: {
+		padding: theme.spacing(2),
+	},
+	listSubHeaderRoot: {
+		lineHeight: "1rem",
+		backgroundColor: "#E5E5E5",
+		color: "#252525",
+	}
+}));
+
+
+export default function Contributors() {
+	const classes = useStyles();
+	const [anchorEl, setAnchorEl] = React.useState(null);
+
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
+
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+
+	const open = Boolean(anchorEl);
+	const id = open ? 'simple-popover' : undefined;
+
+	return (
+		<div>
+			<Button variant="outlined" color="inherit" onClick={handleClick} className={classes.button}>
+				The Developers
+  			</Button>
+			<Popover
+				id={id}
+				open={open}
+				anchorEl={anchorEl}
+				onClose={handleClose}
+				anchorOrigin={{
+					vertical: 'bottom',
+					horizontal: 'center',
+				}}
+				transformOrigin={{
+					vertical: 'top',
+					horizontal: 'center',
+				}}
+			>
+				<PopupInner />
+			</Popover>
+		</div>
+	);
+}
+const PopupInner = () => {
+	const classes = useStyles();
+
+	return (
+		<List dense="true" disablePadding='true'>
+			<ListSubheader classes={{ root: classes.listSubHeaderRoot }}>
+				Concept</ListSubheader>
+			<ListItemText inset='true'>
+				Rud Merriam </ListItemText>
+			<ListSubheader classes={{ root: classes.listSubHeaderRoot }}>
+				Frontend Interface</ListSubheader>
+			<ListItemText inset='true'>
+				Rud Merriam </ListItemText>
+			<ListItemText inset='true'>
+				Kevin Thomson</ListItemText>
+			<ListSubheader classes={{ root: classes.listSubHeaderRoot }}>
+				Backend Processing</ListSubheader>
+			<ListItemText inset='true'>
+				Stephen Ten Eyck</ListItemText>
+			<ListItemText inset='true'>
+				Robert McCutchen</ListItemText>
+			<ListSubheader classes={{ root: classes.listSubHeaderRoot }}>
+				Moral Support</ListSubheader>
+			<ListItemText inset='true' width={'200'}
+			>
+				Mikhail Kozorovitskiy&nbsp;&nbsp;&nbsp;&nbsp;</ListItemText> 
+		</List>
+	)
+}
--- src/Footer.js
+++ src/Footer.js
@@ -1,38 +1,42 @@
 import React from 'react';
 import { Container, makeStyles, Link, Typography } from '@material-ui/core';
+import Contributors from './Contributors';
 
 const Moco = require('./images/moco_logo.png');
 const Wase = require('./images/wase_logo.png');
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    '& > *': {
-      display: 'inline-block',
-      verticalAlign: 'middle',
-    },
-  },
-  footerText: {
-    margin: '0 20px',
-  }
+	root: {
+		'& > *': {
+			display: 'inline-block',
+			verticalAlign: 'middle',
+		},
+	},
+	footerText: {
+		margin: '0 20px',
+	}
 }));
 
 const Footer = () => {
-  const classes = useStyles();
-  return (
-    <Container className={classes.root}>
-      <img src={Moco} alt="Montgomery County Logo" height="50" />
-      <div className={classes.footerText}>
-        <Typography variant="body1" >
-          Web site provided by&nbsp;
-              <Link color="inherit" href="https://www.meetup.com/Woodlands-Area-Software-Enthusiasts">
-            Woodlands Area Software Enthusiasts
-              </Link>
-        </Typography>
-        <Typography variant="body2">Not responsible for accuracy of data. Use with caution.</Typography>
-      </div>
-      <img src={Wase} alt="WASE Logo" height="50" />
-    </Container>
-  )
+	const classes = useStyles();
+	return (
+		<Container className={classes.root}>
+			<img src={Moco} alt="Montgomery County Logo" height="50" />
+			
+			<div className={classes.footerText}>
+				<Typography variant="body1" >
+					Web site provided by&nbsp;
+		              <Link color="inherit" href="https://www.meetup.com/Woodlands-Area-Software-Enthusiasts">
+						Woodlands Area Software Enthusiasts
+		              </Link>
+				</Typography>
+				<Typography variant="body2">Not responsible for accuracy of data. Use with caution.</Typography>
+			</div>
+			
+			<img src={Wase} alt="WASE Logo" height="50" />
+			<Contributors />
+		</Container>
+	)
 };
 
 export default Footer;
